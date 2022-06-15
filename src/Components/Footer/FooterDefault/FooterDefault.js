d294541c2d1750c3685adf85dc51e9a1d92a494a
muafalah <muh.ifa.amrillah@gmail.com>
2022-06-15 20:29:34 +0700

    edit footer default

---
+++ src/Components/Footer/FooterDefault/FooterDefault.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import style from './FooterDefault.module.css'
+
+const FooterDefault = () => {
+    return (
+        <div>FooterDefault</div>
+    )
+}
+
+export default FooterDefault
